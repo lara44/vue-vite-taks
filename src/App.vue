79d98ba67dfa4c23c48dff97d53e56713f3d3f88
lara44 <43456256+lara44@users.noreply.github.com>
2023-08-23 14:37:47 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+       <v-container>
+        <v-card>
+          <v-header>
+            <h1>List of Task</h1>
+          </v-header>
+        </v-card>
+      </v-container>
+  </div>
+</template>
+
